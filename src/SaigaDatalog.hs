7e6d702fc51c48a558b782cd32f0e043d595ecb9
Alexandru Dura <alexandru.dura@cs.lth.se>
2024-04-24 16:03:08 +0200

    Datalog translation for lists

--- src/SaigaDatalog.hs
+++ src/SaigaDatalog.hs
@@ -15,14 +15,13 @@ translateToTermS :: (Enum attr, Eq a) => Expr attr -> State [String] [(SaigaTerm
 translateToTermS (IVal n) = return [(Constant $ DInt n, [])]
 translateToTermS (BVal b) = return [(Constant $ DBool b, [])]
 translateToTermS (SVal s) = return  [(Constant $ DString s, [])]
-translateToTermS Node = return [(Variable "node", [])]
-translateToTermS Arg = return [(Variable "arg", [])]
+translateToTermS Node = return [(Variable "_node", [])]
+translateToTermS Arg = return [(Variable "_arg", [])]
 translateToTermS (IfElse c t f) = do
   -- compute translation within the State monad
   cs <- translateToTermS c
   ts <- translateToTermS t
   fs <- translateToTermS f
-
   return $ do
     -- combine the results in the List monad
     (cvar, cs') <- cs
@@ -30,8 +29,6 @@ translateToTermS (IfElse c t f) = do
     (fvar, fs') <- fs
     [(tvar, [equals cvar (Constant $ DBool True)] ++ cs' ++ ts'),
      (fvar, [equals cvar (Constant $ DBool False)] ++ cs' ++ fs')]
-
-
 
 translateToTermS (Func name arg) = do
   args <- translateToTermS arg
@@ -42,6 +39,37 @@ translateToTermS (Func name arg) = do
            return (fvar, Literal name [fst arg', fvar] id : snd arg'))
     args
 
+translateToTermS (Head e) = do
+  es <- translateToTermS e
+  names <- get
+  let fvar = Variable $ head names
+  put $ tail names
+  return $ [(fvar, Literal "_head" [evar, fvar] id : es') | (evar, es') <- es]
+
+translateToTermS (Tail e) = do
+  es <- translateToTermS e
+  names <- get
+  let fvar = Variable $ head names
+  put $ tail names
+  return $ [(fvar, Literal "_tail" [evar, fvar] id : es') | (evar, es') <- es]
+
+translateToTermS (Cons h t) = do
+  hs <- translateToTermS h
+  ts <- translateToTermS t
+  names <- get
+  let fvar = Variable $ head names
+  put $ tail names
+  return $ do
+    (hvar, hs') <- hs
+    (tvar, ts') <- ts
+    return (fvar, [Literal "_cons" [hvar,  tvar, fvar] id] ++ hs' ++ ts')
+
+translateToTermS Nil = do
+  names <- get
+  let fvar = Variable $ head names
+  put $ tail names
+  return [(fvar, [Literal "_nil" [fvar] id])]
+
 --   fresh <- head varNames
 --   put $ tail varNames
 --   let fvar = Variable fresh
@@ -50,7 +78,11 @@ translateToTermS (Func name arg) = do
 translateToTerm :: (Enum attr, Eq a) => Expr attr -> [(SaigaTerm a, [SaigaAtom a])]
 translateToTerm expr = evalState (translateToTermS expr) freshVarNames
 
-freshVarNames = [p : v | v <- ":":freshVarNames, p <- ['a'..'z'], not $ elem (p : v) ["node", "arg"]]
+reservedNames :: [String]
+reservedNames = ["_node", "_arg", "_nil", "_head", "_tail", "_cons"]
+
+freshVarNames :: [String]
+freshVarNames = ['_' : p : v | v <- "":freshVarNames, p <- ['a'..'z'], ('_' : p : v) `notElem` reservedNames]
 
 
 
