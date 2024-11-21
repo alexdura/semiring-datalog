bab62b8abe86facfbb241e9e6803fdb8596a9d78
Alexandru Dura <alexandru.dura@cs.lth.se>
2024-11-21 14:34:10 +0100

    First attribute for the CFG grammar

--- src/CFGLang.hs
+++ src/CFGLang.hs
@@ -1,11 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module CFGLang where
 
 import AST
+import Saiga
+import Prelude hiding (otherwise)
+
+
 
 terminal s = AST "Terminal" s []
 
 nUse s = AST "NUse" s []
 
-nonEmptySymbolList h@(AST "Terminal" _ _) t@(AST "SymbolList" _ _) = AST "NonEmptySymbolList" "" [h, t]
+nonEmptySymbolList h t = AST "NonEmptySymbolList" "" [h, t]
 
 emptySymbolList = AST "EmptySymbolList" "" []
+
+nonEmptyProdList h t = AST "NonEmptyProdList" "" [h, t]
+
+emptyProdList = AST "EmptyProdList" "" []
+
+nDecl s = AST "NDecl" s []
+
+rule d ps = AST "Rule" "" [d, ps]
+
+cfg = AST "CFG" ""
+
+data CFGAttr = Child
+             | Children
+             | Parent
+             | Name
+             | Kind
+             | First
+             | Follow
+             | Nullable
+             deriving (Eq, Show, Enum)
+
+instance SaigaAttribute CFGAttr
+
+
+firstAttr :: SaigaElement CFGAttr a
+firstAttr =  Attribute First 0 $
+  let parent = Parent <?> []
+      kind = Kind <?> []
+      first  = First <?> []
+  in guard [
+    (
+      Node <.> kind === "EmptySymbolList",
+      Nil
+    ),
+    (
+      Node <.> kind === "NonEmptySymbolList",
+      let symbol = Child <?> [int 0]
+          symbolList = Child <?> [int 1]
+          nullable = Nullable <?> []
+      in
+        IfElse (Node <.> symbol <.> nullable)
+        (Func "union" [Node <.> symbol <.> first, Node <.> symbolList <.> first])
+        (Node <.> symbol <.> first)
+    ),
+    (otherwise, Nil)
+    ]
